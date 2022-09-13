b21db17722dba0906e4d36d57edddb2367fb6e0d
Valeria Petropavlovska <llerriya>
2022-09-13 21:14:07 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page--menu');
-  } else {
-    document.body.classList.remove('page--menu');
-  }
-});
